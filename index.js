f0d30ff92b139f324d7f8a132a5c63207ad98523
D Saunders <devinsaunders709@gmail.com>
2020-02-05 22:01:25 -0800

    passport auth stuff

--- index.js
+++ index.js
@@ -1,8 +1,21 @@
+if (process.env.NODE_ENV !== 'production') {
+  require('dotenv').config();
+}
+
 const express = require('express')
 const app = express();
 const path = require('path')
 const PORT = process.env.PORT || 5000
 const bcrypt = require('bcrypt')
+const passport = require('passport')
+const flash = require('express-flash')
+const session = require('express-session')
+
+const initializePassport = require('./passport-config');
+initializePassport(passport, 
+  (email) => { return users.find(user => user.email === email)},
+  (id) => { return users.find(user => user.id === id)}
+);
 
 const users = [];
 app.use(express.static(path.join(__dirname, 'public')))
@@ -13,12 +26,32 @@ var bodyParser = require('body-parser');
 app.use(bodyParser.json()); 
 app.use(bodyParser.urlencoded({ extended: true }));
 
+// for passport
+app.use(flash());
+app.use(session({
+  secret: process.env.SESSION_SECRET,
+  resave: false,
+  saveUninitialized: false
+}))
+app.use(passport.initialize());
+app.use(passport.session());
+
 app.get('/', (req, res) => {
   res.render('pages/login');
 });
 
 app.get('/login', (req, res) => {
   res.render('pages/login');
+});
+
+app.post('/login', passport.authenticate('local', {
+  successRedirect: '/1',
+  failureRedirect: '/login',
+  failureFlash: true
+}));
+
+app.get('/register', (req, res) => {
+  res.render('pages/register');
 });
 
 app.post('/register', async (req, res) => {
@@ -39,12 +72,6 @@ app.post('/register', async (req, res) => {
   }
   console.log(users);
 });
-
-app.get('/register', (req, res) => {
-  res.render('pages/register');
-});
-
-app.post('/register')
 
 // regex route which accepts integers [1-17]
 app.get('^/:week([1-9]|1[0-7])$', (req, res) => {
